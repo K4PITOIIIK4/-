a9b354206d5e59c38e0d47413b5e37847defdcf2
K4PITOIIIK4 <55291380+K4PITOIIIK4@users.noreply.github.com>
2020-06-18 23:39:39 +0300

    Add files via upload

--- Лабораторная работа 2/ЛР2/ЛР2/Source.cpp
+++ Лабораторная работа 2/ЛР2/ЛР2/Source.cpp
@@ -7,20 +7,19 @@ int main()
 {
 	setlocale(LC_ALL, "RUS");
 
-	double a, b, x1, x2, d, temp, x_max = -INFINITY, x_min = INFINITY, x;
+	double a, b, x1, x2, x3, x4, d, temp, x_max = -INFINITY, x_min = INFINITY, x;
 	bool flag = true;
 	cout << "Начало и конец отсчета." << endl;
 	cin >> x1 >> x2;
-	x = x1;
 	cout << "Шаг:";
 	cin >> d;
 	cout << "Начало и конец нужного интервала." << endl;
 	cin >> a >> b;
 	cout << 'x' << " | " << 'y' << endl;
-
-	for (int i = 0; i <= (x2 - x1) / d; i++, x += d) {
-
-		if (x <= a)
+	x = x1;
+	for (int i = 0; i <= (x2 - x1) / d; i++, x += d) 
+	{
+		if (x < a + 0.00001)
 			temp = abs(x) + sin(x);
 		else
 			if ((a < x) && (x < b)) 
@@ -32,16 +31,18 @@ int main()
 			if (temp < x_min)
 			{
 				x_min = temp;
+				x3 = x;
 			}
 			if (temp > x_max)
 			{
 				x_max = temp;
+				x4 = x;
 			}
 
 		cout << x << " | " << temp  << endl;
 	}
 	cout << endl;
-	cout << "max: " << x_max << "  min: " << x_min << endl;
+	cout << "max: " << x4 << "|" << x_max << "  |  min: " << x3 << "|" << x_min << endl;
 	system("pause");
 	return 0;
 }
